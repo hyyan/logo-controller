263bce12b640005b0307579562dcad339c6c9009
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-10-10 06:35:40 -0700

    Added "printLogo" static method

--- src/HyyanLogoController.php
+++ src/HyyanLogoController.php
@@ -39,6 +39,10 @@ class HyyanLogoController {
         $default = array(
             // path for default logo image relative to the theme dir
             'default' => '/logo.png',
+            //the logo line (default to home page)
+            'url' => home_url('/'),
+            // the logo desciption default to (get_bloginfo('name', 'display')) 
+            'description' => get_bloginfo('name', 'display')
         );
         return apply_filters('Hyyan\LogoController.options', $default);
     }
@@ -54,12 +58,37 @@ class HyyanLogoController {
                 $result : $setting['default'];
     }
 
+    /**
+     * Print logo url
+     * 
+     * @param string $path the url target
+     * @param string $description the logo image description
+     * 
+     * @see HyyanLogoController::getLogoUrl()
+     */
+    public static function printLogo($path = null, $description = null) {
+
+        $setting = static::getOptions();
+
+        $path = !empty($path) ? $path : $setting['url'];
+        $description = !empty($description) ? $description : $setting['description'];
+
+        echo sprintf(
+                '<a href="%1$s" title="%2$s" rel="home">'
+                . '<img src="%3$s" alt="%2$s">'
+                . '</a>'
+                , esc_url($path)
+                , esc_attr($description)
+                , esc_url(static::getLogoUrl())
+        );
+    }
+
 }
 
 /**
  * Get the logo url
  * 
- * @see mlst_Logo_Controller::getgetLogoUrl()
+ * @see HyyanLogoController::getLogoUrl()
  * 
  * @return string
  */
@@ -73,19 +102,8 @@ function hyyan_get_the_logo() {
  * @param string $path the url target
  * @param string $description the logo image description
  * 
- * @see mlst_Logo_Controller::getgetLogoUrl()
+ * @see HyyanLogoController::printLogo
  */
 function hyyan_the_logo($path = null, $description = null) {
-
-    $path = !empty($path) ? $path : home_url('/');
-    $description = !empty($description) ? $description : get_bloginfo('name', 'display');
-
-    echo sprintf(
-            '<a href="%1$s" title="%2$s" rel="home">'
-            . '<img src="%3$s" alt="%2$s">'
-            . '</a>'
-            , esc_url($path)
-            , esc_attr($description)
-            , esc_url(hyyan_get_the_logo())
-    );
+    HyyanLogoController::printLogo($path, $description);
 }
