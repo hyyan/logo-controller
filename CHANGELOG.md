65e7af3acec4ea4d44b79e7ff686e7f12cc0670c
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-10-14 10:17:02 -0700

    Prevented direct access for the plugin file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+###0.2.1
+
+* Prevented direct access for the plugin file
+
 ###0.2
 
 * Added support for "github-updater" plugin
--- __init__.php
+++ __init__.php
@@ -5,13 +5,15 @@
  * Plugin URI: https://github.com/hyyan/logo-controller/
  * Description: Wordpress plugin to add new option for the theme customizer allowing the user to change the logo
  * Author: Hyyan Abo Fakher
- * Version: 0.2
+ * Version: 0.2.1
  * Author URI: https://github.com/hyyan
  * GitHub Plugin URI: hyyan/logo-controller
  * Domain Path: /languages
  * Text Domain: logo-controller
  * License: MIT License
  */
+
+if (!defined('ABSPATH')) exit('restricted access');
 
 require_once __DIR__ . '/src/HyyanLogoController.php';
 require_once __DIR__ . '/src/functions.php';
